c2c7af194e1bff67786158819a213eb831aff433
Anna_Maslova <personnel2003@yandex.ru>
2022-09-21 00:30:24 +0300

    Program

---
+++ Program/Program.cs
@@ -0,0 +1,24 @@
+﻿void Demo(string [] Demoarray)
+{
+    Console.Write("[");
+    for (int j=0; j<Demoarray.Length; j++)
+    {
+        Console.Write(Demoarray[j] + " ");
+    }
+    Console.Write("]");
+}
+
+string[] array = {"123", "Hello", "world", ":-)"};
+int lenArray = array.Length;
+string[] array1 = new string [2];
+int j=0;
+for(int i =0; i < lenArray; i++)
+{
+    int lenString = array[i].Length;
+        if (lenString <= 3)
+    {
+       array1[j] = array[i];
+       j++; 
+    }
+}
+Demo(array1);
